b49fd5f3f8780ec24e04170632fdbb7e61c6de40
Coleman Engbrecht <linuxFTW49@gmail.com>
2017-12-06 23:58:25 -0700

    modified type_info and removed constexpr from ptr

--- ptr.h
+++ ptr.h
@@ -5,35 +5,35 @@
 
 namespace oak::ptr {
 
-	constexpr inline void* add(void* p, size_t x) {
+	inline void* add(void* p, size_t x) {
 		return reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(p) + x);
 	}
 
-	constexpr inline const void* add(const void* p, size_t x) {
+	inline const void* add(const void* p, size_t x) {
 		return reinterpret_cast<const void*>(reinterpret_cast<uintptr_t>(p) + x);
 	}
 
-	constexpr inline void* subtract(void* p, size_t x) {
+	inline void* subtract(void* p, size_t x) {
 		return reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(p) - x);
 	}
 
-	constexpr inline const void* subtract(const void* p, size_t x) {
+	inline const void* subtract(const void* p, size_t x) {
 		return reinterpret_cast<const void*>(reinterpret_cast<uintptr_t>(p) - x);
 	}
-	constexpr inline size_t align_size(size_t size, uint32_t alignment) {
+	inline size_t align_size(size_t size, uint32_t alignment) {
 		return (size + alignment-1) & (~(alignment-1));
 	}
 
-	constexpr inline uint32_t align_offset(const void *address, uint32_t alignment) {
+	inline uint32_t align_offset(const void *address, uint32_t alignment) {
 		uint32_t adjustment = (alignment - (reinterpret_cast<uintptr_t>(address) & static_cast<uintptr_t>(alignment - 1)));
 		return adjustment == alignment ? 0 : adjustment;
 	}
 
-	constexpr inline void* align_address(void *address, uint32_t alignment) {
+	inline void* align_address(void *address, uint32_t alignment) {
 		return add(address, align_offset(address, alignment));
 	}
 
-	constexpr inline uint32_t align_offset_with_header(const void *address, uint32_t alignment, uint32_t headerSize) {
+	inline uint32_t align_offset_with_header(const void *address, uint32_t alignment, uint32_t headerSize) {
 		uint32_t adjustment = align_offset(address, alignment);
 
 		uint32_t neededSpace = headerSize;
--- type_info.h
+++ type_info.h
@@ -1,145 +1,119 @@
 #pragma once
 
-#include <cstring>
+#include <cinttypes>
+#include <cstddef>
 
 #include "string.h"
 #include "ptr.h"
 
-#include "../conf.h"
+#ifdef __OSIG__
+#define _reflect(x) __attribute__((annotate("reflect:"#x)))
+#else
+#define _reflect(x)
+#endif
 
 namespace oak {
 
-	struct Type {
-		enum Enum {
-			VOID,
-			INT8,
-			INT16,
-			INT32,
-			INT64,
-			UINT8,
-			UINT16,
-			UINT32,
-			UINT64,
-			FLOAT32,
-			FLOAT64,
-			STRING,
-			STRUCT,
-		};
+	enum class TypeKind {
+		VOID,
+		UINT8,
+		UINT16,
+		UINT32,
+		UINT64,
+		INT8,
+		INT16,
+		INT32,
+		INT64,
+		FLOAT32,
+		FLOAT64,
+		PTR,
+		ARRAY,
+		STRUCT,
+		ENUM,
 	};
 
 	struct TypeInfo {
-		Type::Enum type;
+		TypeKind kind;
 		String name;
-		size_t size;
-		size_t align;
+		size_t size = 0;
+		size_t align = 0;
 	};
 
-	struct VarFlag { 
-		enum Enum {
-			POINTER = 0x01,
-			CONST = 0x02,
-			VOLATILE = 0x04,
-		};
+	struct PtrInfo : TypeInfo {
+		const TypeInfo *of = nullptr;
 	};
 
-	struct Var {
-		const TypeInfo *info;
+	struct ArrayInfo : TypeInfo {
+		const TypeInfo *of = nullptr;
+		size_t count = 0;
+	};
+
+	struct VarInfo {
+		const TypeInfo *type = nullptr;
 		String name;
-		uint32_t version = 0; //first version this var appeared in
 		uint32_t flags = 0;
-		uint32_t count = 0; //greater than zero if var is an array
+		uint64_t version = 0;
 	};
 
-	struct Any : Var {
-		void *ptr;
+	struct Any : VarInfo {
+		void *ptr = nullptr;
 	};
 
 	struct StructInfo : TypeInfo {
-		size_t tid = 0;
-		struct {
-			struct VarMemberList {
-				struct Iterator {
-					const Var *var; //the current variable
-					void *ptr;
+		struct MemberList {
+			struct Iterator {
+				const VarInfo *var;
+				void *ptr;
 
-					inline Any operator*() const {
-						return { { *var }, ptr };
-					}
-					inline Iterator& operator++() { 
-						if (var->flags & VarFlag::POINTER) {
-							ptr = ptr::add(ptr, sizeof(void*));
-						} else {
-							ptr = ptr::add(ptr, var->info->size);
-						}
-						var++;
-						if (var && var->info) {
-							size_t a;
-							if (var->flags & VarFlag::POINTER) {
-								a = alignof(void*);
-							} else {
-								a = var->info->align;
-							}
-							ptr = ptr::align_address(ptr, a);
-						}
-						return *this;
-					}
-
-					inline bool operator!=(const Iterator& other) const {
-						return var != other.var;
-					}
-
-				};
-				const Var *var;
-				size_t count; //the number of vars in the list
-				void *ptr; //the pointer to the struct
-
-				inline const Iterator begin() const {
-					return Iterator{ var, ptr };
+				inline Any operator*() const {
+					return { { *var }, ptr };
 				}
-				inline const Iterator end() const {
-					return Iterator{ var + count, ptr };
+
+				inline Iterator& operator++() {
+					ptr = ptr::add(ptr, var->type->size);
+					var++;
+					if (var && var->type) {
+						ptr = ptr::align_address(ptr, var->type->align);
+					}
+					return *this;
+				}
+
+				inline bool operator!=(const Iterator& other) {
+					return var != other.var;
 				}
 			};
-			const Var *data = nullptr;
-			size_t count = 0;
 
-			inline VarMemberList operator()(void *pm) const {
-				return VarMemberList{ data, count, pm };
-			}
+			const VarInfo *var;
+			size_t count;
+			void *ptr;
 
-			inline const Var* begin() const { return data; }
-			inline const Var* end() const { return data + count; }
-		} members;
+			inline Iterator begin() const { return { var, ptr }; }
+			inline Iterator end() const { return { var + count, ptr }; }
+		};
+
+		const VarInfo *members = nullptr;
+		size_t memberCount = 0;
+		size_t tid = 0;
+
+		MemberList operator()(void *ptr) const {
+			return { members, memberCount, ptr };
+		}
+
+		inline const VarInfo* begin() const { return members; }
+		inline const VarInfo* end() const { return members + memberCount; }
 	};
 
+	struct EnumInfo : TypeInfo {
+		const String *members = nullptr;
+		size_t memberCount = 0;
 
-	template<typename T>
-	constexpr StructInfo make_struct_info(size_t id, const String& name) {
-		return StructInfo { 
-			{ Type::STRUCT, name, sizeof(T), alignof(T) }, id
-		};
-	}
+		inline const String* begin() const {
+			return members;
+		}
 
-	template<typename T, size_t N>
-	constexpr StructInfo make_struct_info(size_t id, const String& name, const Var (&array)[N]) {
-		return StructInfo { 
-			{ Type::STRUCT, name, sizeof(T), alignof(T) }, id,
-			{ &array[0], N }
-		};
-	}
-
-	inline TypeInfo types[] = {
-		{ Type::VOID, "void", 0, 0 },
-		{ Type::INT8, "int8", 1, alignof(int8_t) },
-		{ Type::INT16, "int16", 2, alignof(int16_t) },
-		{ Type::INT32, "int32", 4, alignof(int32_t) },
-		{ Type::INT64, "int64", 8, alignof(int64_t) },
-		{ Type::UINT8, "uint8", 1, alignof(uint8_t) },
-		{ Type::UINT16, "uint16", 2, alignof(uint16_t) },
-		{ Type::UINT32, "uint32", 4, alignof(uint32_t) },
-		{ Type::UINT64, "uint64", 8, alignof(uint64_t) },
-		{ Type::FLOAT32, "float32", 4, alignof(float) },
-		{ Type::FLOAT64, "float64", 8, alignof(double) },
-		{ Type::STRING, "string", 16, alignof(String) },
+		inline const String* end() const {
+			return members + memberCount; 
+		}
 	};
-} 
+
+}
