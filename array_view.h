2a3acf8ec6390ebda2bf81f25980582309ee0e07
Coleman Engbrecht <linuxFTW49@gmail.com>
2018-02-10 14:29:56 -0700

    added constructors to ArrayView:

--- array_view.h
+++ array_view.h
@@ -9,6 +9,11 @@ namespace oak {
 		T *data = nullptr;
 		size_t size = 0;
 
+		ArrayView() = default;
+		ArrayView(T *_data, size_t _size) : data{ _data }, size{ _size } {}
+		template<size_t C>
+		ArrayView(T array&[C]) : data{ &array }, size{ C } {}
+
 		T& operator[](size_t idx) { return data[idx]; }
 		const T& operator[](size_t idx) const { return data[idx]; }
 
