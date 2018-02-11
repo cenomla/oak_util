f79139204242ff2703137c4cbe444ea0faa41c2a
Coleman Engbrecht <linuxFTW49@gmail.com>
2018-02-10 19:14:49 -0700

    added conversion operator from array to array view

--- array.h
+++ array.h
@@ -4,6 +4,7 @@
 #include <cassert>
 
 #include "allocator.h"
+#include "array_view.h"
 
 namespace oak {
 
@@ -104,6 +105,8 @@ namespace oak {
 
 		T& operator[](size_t idx) { return data[idx]; }
 		const T& operator[](size_t idx) const { return data[idx]; }
+
+		operator ArrayView<T>() const { return { data, size }; }
 
 		inline T* begin() { return data; }
 		inline T* end() { return data + size; }
