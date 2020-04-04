547b76a6a4c724a82980d8e0996fe9e56344e67c
Coleman Engbrecht <linuxFTW49@gmail.com>
2020-04-04 11:47:13 -0600

    Fix include guard

--- include/oak_util/types.h
+++ include/oak_util/types.h
@@ -41,6 +41,7 @@ using byte = unsigned char;
 #define MACRO_CAT(x, y) MACRO_CAT_IMPL(x, y)
 
 #ifndef __OSIG_REFLECT_MACRO__
+#define __OSIG_REFLECT_MACRO__
 
 #ifdef __OSIG__
 #define _reflect(...) __attribute__((annotate("reflect;" #__VA_ARGS__)))
